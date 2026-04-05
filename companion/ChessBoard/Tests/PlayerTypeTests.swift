7d93c420b63cc2fe38ea65d44b39a0904168a694
Jonas Damtoft <Jomik@users.noreply.github.com>
2026-04-05 18:24:46 +0200

    feat(companion): add lichessAi player type with level parameter

--- companion/ChessBoard/Tests/PlayerTypeTests.swift
+++ companion/ChessBoard/Tests/PlayerTypeTests.swift
@@ -14,5 +14,27 @@ final class PlayerTypeTests: XCTestCase {
     func testEncodedLengths() {
         XCTAssertEqual(PlayerType.human.encode().count, 1)
         XCTAssertEqual(PlayerType.embedded.encode().count, 1)
+        XCTAssertEqual(PlayerType.lichessAi.encode(level: 1).count, 2)
+    }
+
+    func testEncodeLichessAiLevel1() {
+        XCTAssertEqual(
+            PlayerType.lichessAi.encode(level: 1),
+            Data([0x02, 0x01])
+        )
+    }
+
+    func testEncodeLichessAiLevel8() {
+        XCTAssertEqual(
+            PlayerType.lichessAi.encode(level: 8),
+            Data([0x02, 0x08])
+        )
+    }
+
+    func testEncodeLichessAiLevel4() {
+        XCTAssertEqual(
+            PlayerType.lichessAi.encode(level: 4),
+            Data([0x02, 0x04])
+        )
     }
 }
